b64ecf7508415c475e8361db8c8a4444f4598182
ismael <spacingismael@gmail.com>
2024-06-25 23:17:08 +0100

    fs fixing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,6 +59,6 @@ class FileStorage:
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
-            key = (obj.__class__.__name__ + '.' + obj.id) 
+            key = (obj.__class__.__name__ + '.' + obj.id)
             if key in self.__objects.keys():
                 del self.__objects[key]
